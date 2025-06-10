70356281b3c263eb392a2b8777e4d7bdf91f999c
PauloMilhomem <paulinhomilhomem123@gmail.com>
2025-06-10 20:26:45 -0300

    Update settings.py

--- PrjBibliotech/settings.py
+++ PrjBibliotech/settings.py
@@ -128,14 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "sitecrud/static",
-]
-
-# Pasta onde os arquivos vão quando você rodar collectstatic
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Pasta para coletar arquivos estáticos
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
