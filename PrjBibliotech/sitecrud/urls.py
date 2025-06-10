d4b3703217a42182d268ef7037b4e06e61a9579f
PauloMilhomem <paulinhomilhomem123@gmail.com>
2025-06-10 18:49:17 -0300

    Delete PrjBibliotech directory

--- PrjBibliotech/sitecrud/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path("sistema-restrito-crud/", views.sistema_restrito_crud, name="sistema-restrito-crud"),
-    path("sistema-restrito-login/", views.sistema_restrito_login, name="sistema-restrito-login"),
-]
